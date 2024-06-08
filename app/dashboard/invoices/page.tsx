78effaef7fd1dfd1a6786294074a9238a5805dcc
Carlos Augusto <augustomonoo@gmail.com>
2024-06-07 23:40:17 -0300

    Up to chapter 16

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -6,6 +6,12 @@ import Table from "@/app/ui/invoices/table";
 import Pagination from "@/app/ui/invoices/pagination";
 import Search from "@/app/ui/search";
 import { fetchInvoicesPages } from "@/app/lib/data";
+import { Metadata } from "next";
+
+
+export const metadata: Metadata ={
+  title: "Invoices"
+}
 
 export default async function Page({
   searchParams,
